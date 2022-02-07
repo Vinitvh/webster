92ef7febc1ccd662d3d97aba000a1434234e8175
Vinit Hemadri <vineet.vh@gmail.com>
2022-02-07 14:27:45 +0530

    Added navbar component and theme changer button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Routes from "./components/Routes";
@@ -7,13 +8,15 @@ const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-        <Navbar />
-        <Routes />
-        <Footer />
+    <Router>
+      <div className={darkTheme ? "dark" : ""}>
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+          <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+          <Routes />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FcSearch } from "react-icons/fc";
+import Search from "./Search";
 
-function Navbar() {
-  return <div>Navbar</div>;
+function Navbar({ darkTheme, setDarkTheme }) {
+  return (
+    <div
+      className="p-5 pb-auto flex flex-wrap 
+            sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200"
+    >
+      <div className="flex justify-between items-center space-x-5 w-screen">
+        <Link to="/">
+          <p
+            className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 
+                  dark:text-gray-900"
+          >
+            Webster <FcSearch className="inline" />
+          </p>
+        </Link>
+        <button
+          type="button"
+          onClick={() => setDarkTheme(!darkTheme)}
+          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white rounded-full px-2 py-1 hover:shadow-lg"
+        >
+          {darkTheme ? "💡" : "🌙"}
+        </button>
+      </div>
+      <Search />
+    </div>
+  );
 }
 
 export default Navbar;
