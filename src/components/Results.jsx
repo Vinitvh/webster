f29736655bbac27d2367e4ef078746c01c06c831
Vinit Hemadri <vineet.vh@gmail.com>
2022-02-08 21:36:55 +0530

    Completed implementing search results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,14 +10,30 @@ function Results() {
   const location = useLocation();
 
   useEffect(() => {
-    getResults("?hl=en&q=Javascript&gl=in");
+    getResults("/search/q=javascript&num=40");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (loading) return <Loading />;
 
   switch (location.pathname) {
     case "/search":
-      return "Search";
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+          {results?.results?.map(({ link, title }, idx) => (
+            <div key={idx} className="md:w-2/5 w-full">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm mb-0">
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className="text-lg mb-0 hover:underline dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
     case "/images":
       return "Search";
     case "/news":
--- src/context/ResultsContext.js
+++ src/context/ResultsContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-const baseURL = "https://google-search1.p.rapidapi.com/google-search";
+const baseURL = "https://google-search3.p.rapidapi.com/api/v1";
 const ResultsContext = createContext();
 
 export const ResultContextProvider = ({ children }) => {
@@ -10,18 +10,24 @@ export const ResultContextProvider = ({ children }) => {
 
   const getResults = async (text) => {
     setloading(true);
+    try {
+      const response = await fetch(`${baseURL}${text}`, {
+        method: "GET",
+        headers: {
+          "x-user-agent": "desktop",
+          "x-proxy-location": "IN",
+          "x-rapidapi-host": "google-search3.p.rapidapi.com",
+          "x-rapidapi-key":
+            "697417e7edmshdd0523d0471951fp1c923ajsn93dbe43fcf84",
+        },
+      });
 
-    const response = await fetch(`${baseURL}${text}`, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "google-search1.p.rapidapi.com",
-        "x-rapidapi-key": "697417e7edmshdd0523d0471951fp1c923ajsn93dbe43fcf84",
-      },
-    });
-
-    const data = await response.json();
-    setResults(data);
-    setloading(false);
+      const data = await response.json();
+      setResults(data);
+      setloading(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
